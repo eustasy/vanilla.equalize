9c21e92a7297828da9081332c6edcfcf92436cb9
Lewis Goddard <lewisgoddard@eustasy.org>
2017-05-02 22:19:50 +0100

    Remove semi-colons

--- vanilla.equalize.js
+++ vanilla.equalize.js
@@ -5,61 +5,61 @@
  */
 
 (function (exports, augment, $) {
-	'use strict';
-	augment(Array, ['forEach']);
+	'use strict'
+	augment(Array, ['forEach'])
 
 	/**
 	 * This function is (almost) exactly the same
 	 * as in jQuery.equalize (modulo a few tweaks)
 	 */
 	exports.equalize = function (group, equalize) {
-		group = group || '.group';
-		equalize = equalize || '.equalize';
+		group = group || '.group'
+		equalize = equalize || '.equalize'
 		$(group).each(function () {
-			var highestBox = 0;
+			var highestBox = 0
 
 			$(equalize, this).
 				css('height', 'auto').
 				each(function () {
 					if ($(this).innerHeight() > highestBox) {
-						highestBox = $(this).innerHeight();
+						highestBox = $(this).innerHeight()
 					}
 				}).
-				innerHeight(highestBox);
-		});
-	};
+				innerHeight(highestBox)
+		})
+	}
 
 	// When the page has loaded
 	$(window).on('DOMContentLoaded', function () {
-		var resizeTimeout;
-		equalize(); // Run the function
+		var resizeTimeout
+		equalize() // Run the function
 
 		// And every time the window is resized
 		$(window).on('resize', function () {
-			if (resizeTimeout) return;
+			if (resizeTimeout) return
 
 			// Will execute at a rate of 15fps
 			resizeTimeout = setTimeout(function() {
-				resizeTimeout = null;
-				equalize(); // Run again
-			}, 66);
-		});
-	});
+				resizeTimeout = null
+				equalize() // Run again
+			}, 66)
+		})
+	})
 
 })(window,
 /**
  * Augment Class with some of its prototype
  */
 function augment (Class, keys) {
-	'use strict';
-	var slice = Array.prototype.slice;
+	'use strict'
+	var slice = Array.prototype.slice
 	keys.forEach(function (key) {
-		var method = Class.prototype[key];
+		var method = Class.prototype[key]
 		Class[key] = function () {
-			var args = slice.call(arguments, 1);
-			method.apply(arguments[0], args);
-		};
-	});
+			var args = slice.call(arguments, 1)
+			method.apply(arguments[0], args)
+		}
+	})
 }, window.jQuery ||
 /**
  * Here comes a teeny-tiny jQuery shim (just because
@@ -69,50 +69,50 @@ function augment (Class, keys) {
  * included before this one.
  */
 function jQuery (sel, ctx) {
-	'use strict';
+	'use strict'
 	switch (typeof sel) {
 		case 'string':
-			ctx = ctx || document;
-			sel = ctx.querySelectorAll(sel);
+			ctx = ctx || document
+			sel = ctx.querySelectorAll(sel)
 			return {
 				css: function (key, val) {
 					Array.forEach(sel, function each (item) {
-						item.style[key] = val;
-					});
-					return this;
+						item.style[key] = val
+					})
+					return this
 				},
 				each: function (callback) {
 					Array.forEach(sel, function each (item, index) {
-						callback.call(item, index, item);
-					});
-					return this;
+						callback.call(item, index, item)
+					})
+					return this
 				},
 				innerHeight: function (val) {
 					Array.forEach(sel, function each (item) {
-						item.style.height = val + 'px';
-					});
-					return this;
+						item.style.height = val + 'px'
+					})
+					return this
 				}
-			};
+			}
 
 		case 'object':
 			if (sel == null) {
-				throw 'Bad usage of $ - the first argument is null';
+				throw 'Bad usage of $ - the first argument is null'
 			}
 			return {
 				innerHeight: function () {
-					return sel.clientHeight;
+					return sel.clientHeight
 				},
 				on: function (event, handler) {
-					sel.addEventListener(event, handler);
-					return this;
+					sel.addEventListener(event, handler)
+					return this
 				}
 			}
 
 		case 'undefined':
-			throw 'Bad usage of $ - nothing given as first argument';
+			throw 'Bad usage of $ - nothing given as first argument'
 
 		default:
-			throw "$ doesn't currently work on a " + (typeof sel);
+			throw "$ doesn't currently work on a " + (typeof sel)
 	}
-});
+})
